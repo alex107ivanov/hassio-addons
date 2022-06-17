4fc600ae058ed228bfc8ff9fa7bea51f03a08965
Aleksey Ivanov <alex@odbo.co>
2022-06-17 17:24:17 +0300

    One more version: config parse problems

--- openvpn-client/entrypoint.sh
+++ openvpn-client/entrypoint.sh
@@ -8,14 +8,9 @@ if [ ! -c /dev/net/tun ]; then
 	mknod /dev/net/tun c 10 200
 fi
 
-OVPNCONF=${OVPNCONF:-/vpn/config/config.ovpn}
+OVPNCONF='/vpn/config/config.ovpn'
 
-cat /data/options.json | jq -r '.config' > ${OVPNCONF}
-
-[ -f $OVPNCONF ] || {
-	echo $OVPNCONF is missing please provide the volume with the file  > /dev/stderr;
-	exit 1;
-}
+cat /data/options.json | jq -r '.config' > $OVPNCONF
 
 exec /usr/sbin/openvpn --config $OVPNCONF
 
